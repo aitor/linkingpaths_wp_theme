3aa8f91b7bb54ff6544f3a1f5050272bdb27af24
Aitor García <aitor.garcia@gmail.com>
2009-01-26 13:25:02 +0100

    Categories listing replaced by tags listing

--- the_excerpt.php
+++ the_excerpt.php
@@ -2,8 +2,8 @@
   <div class="title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></div>
   <div class="metainfo">
     <span class="meta author">por <a href="#"><?php the_author() ?></a></span> -
-    <span class="meta date"><strong><?php the_time(__('F jS, Y', 'kubrick')) ?></strong></span>
-    <span class="meta tags"><?php printf(__('Posted in %s', 'kubrick'), get_the_category_list(', ')); ?></span> 
+    <span class="meta date"><strong><?php the_time(__('F jS, Y', 'kubrick')) ?></strong></span> -
+    <span class="meta tags"><?php echo  get_the_tag_list('Etiquetado como ',', ',''); ?></span>
   </div> <!-- /.metainfo -->
   <span class="comments_link"><?php comments_popup_link(__('No Comments &#187;', 'kubrick'), __('1 Comment &#187;', 'kubrick'), __('% Comments &#187;', 'kubrick'), '', __('Comments Closed', 'kubrick') ); ?></span>
   <?php if (function_exists('sharethis_button')) { sharethis_button(); } ?>          
